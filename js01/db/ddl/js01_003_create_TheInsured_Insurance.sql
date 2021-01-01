c7f2aa2eaef812e87fdd31e51a033bdc8702b75b
wu2wa <2639485878@qq.com>
2021-01-01 09:44:40 +0800

    004 Insurance

--- js01/db/ddl/js01_003_create_TheInsured_Insurance.sql
+++ js01/db/ddl/js01_003_create_TheInsured_Insurance.sql
@@ -1,14 +1,18 @@
-CREATE TABLE `TheInsured` (
+CREATE TABLE `theinsured` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '被保险人id',
-  `corporateName` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '被保险人',
-  `corporateAddress` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT '保险公司地址',
-  `phoneNumber` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '联系电话',
-  `postalCode` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT ' 邮政编码',
-
+  `InsuredName` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '被保险人',
+  `InsuredIdNumber` varchar(18) COLLATE utf8_unicode_ci NOT NULL COMMENT '被保险人证件号码',
+  `DriverSLicense` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '行驶证车主',
+  `address` varchar(11) COLLATE utf8_unicode_ci NOT NULL COMMENT ' 地址',
+  `telephoneNumber` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '联系电话',
   `status` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '状态 自定义使用',
   `created_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `created_by` varchar(20) COLLATE utf8_unicode_ci NULL COMMENT '创建人 ',
+  `created_by` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人 ',
   `updated_time` datetime DEFAULT NULL COMMENT '更新时间',
-  `updated_by` varchar(20) COLLATE utf8_unicode_ci NULL COMMENT '更新人',
+  `updated_by` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '更新人',
   PRIMARY KEY (`id`),
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `InsuredIdNumber` (`InsuredIdNumber`),
+  UNIQUE KEY `InsuredName` (`InsuredName`),
+  UNIQUE KEY `DriverSLicense` (`DriverSLicense`),
+  UNIQUE KEY `telephoneNumber` (`telephoneNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
