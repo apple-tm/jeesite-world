849bf7d9e37590180216a40d398aca7e1902b63b
wu2wa <2639485878@qq.com>
2021-01-09 19:04:47 +0800

    06 Insurance

---
+++ js01/src/main/java/com/example/js01/util/ObjectToJSONObjUtil.java
@@ -0,0 +1,12 @@
+//package com.example.js01.util;
+//
+//import com.alibaba.fastjson.JSONObject;
+//
+//public class ObjectToJSONObjUtil {
+//
+//    public static<T> JSONObject executeTrans(T t) {
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.
+//        return null;
+//    }
+//}
